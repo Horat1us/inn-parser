89ad9cddbd5274f4e4a68b691711619cdbae25a9
Alexander Letnikow <reclamme@gmail.com>
2018-06-05 13:38:01 +0300

    Add Gender interface with male/female constants

---
+++ src/Gender.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Horat1us\Inn;
+
+/**
+ * Interface Gender
+ * @package Horat1us\Inn
+ */
+interface Gender
+{
+    public const MALE = 'male';
+    public const FEMALE = 'female';
+}
